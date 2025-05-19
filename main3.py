7158d06b4af9cc5f6d62102b3113bc2976a8fa20
Alexa1883 <sashashem62@gmail.com>
2025-05-19 16:28:10 +0700

    Update main3.py

--- main3.py
+++ main3.py
@@ -1,11 +1,35 @@
-class Car:
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self.year = year
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.__balance = initial_balance
 
-my_car = Car("Toyota", "Camry", 2020)
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+            print(f"Депозит на {amount} руб. Успешно!")
+        else:
+            print("Ошибка: сумма депозита должна быть положительной")
 
-print(f"Марка: {my_car.make}")
-print(f"Модель: {my_car.model}")
-print(f"Год выпуска: {my_car.year}")
+    def withdraw(self, amount):
+        if amount > 0:
+            if self.__balance >= amount:
+                self.__balance -= amount
+                print(f"Снятие {amount} руб. Успешно!")
+            else:
+                print("Ошибка: недостаточно средств")
+        else:
+            print("ошибкаа: сумма снятия должна быть положительной")
+
+    def get_balance(self):
+        return self.__balance
+
+account = BankAccount(1000)
+print("Текущий баланс: ", account.get_balance())
+
+account.deposit(500)
+print("Текущий баланс: ", account.get_balance())
+
+account.withdraw(150)
+print("Текущий баланс: ", account.get_balance())
+
+account.withdraw(2000)
+account.withdraw(-100)
