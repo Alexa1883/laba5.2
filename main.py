92c9dddc2b2f50106fad01ee084ced003d271042
Alexa1883 <sashashem62@gmail.com>
2025-05-19 16:27:18 +0700

    Update main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+my_car = Car("Toyota", "Camry", 2020)
+
+print(f"Марка: {my_car.make}")
+print(f"Модель: {my_car.model}")
+print(f"Год выпуска: {my_car.year}")
